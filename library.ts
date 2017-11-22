e2372ca92f751654e19fecc474ca258cec8ab217
snowyyu <731444920@qq.com>
2017-11-22 18:43:37 +0800

    增加1.对象深复制2.转化数组为键值对对象

--- library.ts
+++ library.ts
@@ -2,12 +2,6 @@ import { Injectable } from '@angular/core'
 @Injectable()
 export class LibraryService{
 	//函数库
-
-
-
-	
-	
-	
 	
 	/**
 	 * name:生成随机不重复值的方法
@@ -20,5 +14,49 @@ export class LibraryService{
 		let index=Math.floor(Math.random()*arr.length);
 		return arr.splice(index,1)[0]
 	}
+
+	/**
+	 * name:深复制的方法
+	 * description:
+	 * others:
+	 * @param  {Object}
+	 * @return {any}
+	 */
+	cloneObj(obj:Object):any{
+	    let str, newobj = obj.constructor === Array ? [] : {};
+	    if(typeof obj !== 'object'){
+	        return;
+	    } else if(JSON){
+	        str = JSON.stringify(obj), //系列化对象
+	        newobj = JSON.parse(str); //还原
+	    } else {
+	        for(let i in obj){
+	            newobj[i] = typeof obj[i] === 'object' ? 
+	            this.cloneObj(obj[i]) : obj[i]; 
+	        }
+	    }
+	    return newobj;
+	};
+
+
+	/**
+	 * name:将数组转化为键值对的方法
+	 * description:
+	 * others:
+	 * @param {any[]}
+	 * @param {string}
+	 */
+	changeToKeyValue(arr:any[],key:string){
+		let keyValueObj={}
+		if (arr instanceof Array) {
+			for(let item of arr){
+				keyValueObj[item.key]=item
+			}
+			return keyValueObj
+		}else{
+			return
+		}
+	}
+
 
 }
